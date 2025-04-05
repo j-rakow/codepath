77123d969d6e1bbbfb4c82050763f3d0cb8ab2ff
christina-faye <198244416+christina-faye@users.noreply.github.com>
2025-04-04 14:57:25 -1000

    obj deconstruction for coin info props

--- crypto-hustle/src/components/CoinInfo.jsx
+++ crypto-hustle/src/components/CoinInfo.jsx
@@ -3,12 +3,12 @@ import "./CoinInfo.css";
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
-function CoinInfo(props) {
+function CoinInfo({image, name, symbol}) {
   const [price, setPrice] = useState(null)
 
   useEffect(() => {
     const getCoinPrice = async () => {
-      let response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${props.symbol}&tsyms=USD&api_key=${API_KEY}`);
+      let response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=${API_KEY}`);
       let responseJson = await response.json();
       setPrice(responseJson);
     }
@@ -18,9 +18,9 @@ function CoinInfo(props) {
   return (
     <div>
       {price && (
-        <div className="main-list" key={props.symbol}>
-          <img className="icons" src={`https://www.cryptocompare.com${props.image}`} alt={`Small icon for ${props.name} crypto coin`}/>
-          {`${props.name} (${props.symbol})`}
+        <div className="main-list" key={symbol}>
+          <img className="icons" src={`https://www.cryptocompare.com${image}`} alt={`Small icon for ${name} crypto coin`}/>
+          {`${name} (${symbol})`}
           <span className="tab">
             ${price.USD} USD
           </span>
